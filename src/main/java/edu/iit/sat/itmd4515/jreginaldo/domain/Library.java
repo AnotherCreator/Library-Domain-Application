8d47a8a326d70ce68ec60da1708e915ba172ec0a
AnotherCreator <jreginaldo@protonmail.com>
2022-09-25 10:41:46 -0500

    [Lab 4] Create initial library POJO

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Library.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Library.java
@@ -1,4 +1,33 @@
 package edu.iit.sat.itmd4515.jreginaldo.domain;
 
 public class Library {
+
+    private String name;
+    private String streetAdd;
+    private String phoneNum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreetAdd() {
+        return streetAdd;
+    }
+
+    public void setStreetAdd(String streetAdd) {
+        this.streetAdd = streetAdd;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
 }
