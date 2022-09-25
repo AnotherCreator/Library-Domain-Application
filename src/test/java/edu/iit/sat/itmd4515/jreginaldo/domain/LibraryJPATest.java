044cff9855f612308223c1efbefa9d1f8cff906c
AnotherCreator <jreginaldo@protonmail.com>
2022-09-25 12:08:15 -0500

    [Lab 4] Create basic test structure

--- src/test/java/edu/iit/sat/itmd4515/jreginaldo/domain/LibraryJPATest.java
+++ src/test/java/edu/iit/sat/itmd4515/jreginaldo/domain/LibraryJPATest.java
@@ -1,4 +1,46 @@
 package edu.iit.sat.itmd4515.jreginaldo.domain;
 
+import org.junit.jupiter.api.*;
+
 public class LibraryJPATest {
+
+    @BeforeAll
+    public static void beforeAll() {
+
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+
+    }
+
+    @Test
+    public void createTest() {
+
+    }
+
+    @Test
+    public void readTest() {
+
+    }
+
+    @Test
+    public void updateTest() {
+
+    }
+
+    @Test
+    public void deleteTest() {
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+
+    }
+
+    @AfterAll
+    public static void afterAll() {
+
+    }
 }
