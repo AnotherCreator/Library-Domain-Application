06bd16c5d067e8b04d840ac593312c83182698a3
AnotherCreator <jreginaldo@protonmail.com>
2022-09-25 12:00:35 -0500

    [Lab 4] Add database entity managers

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/demo/Demo.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/demo/Demo.java
@@ -2,15 +2,24 @@ package edu.iit.sat.itmd4515.jreginaldo.demo;
 
 import edu.iit.sat.itmd4515.jreginaldo.domain.Library;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Demo {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("itmd4515testPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
         Library library = new Library();
 
         library.setName("Avant-Garde Athenaeum");
         System.out.println(library);
+
+        em.persist(library);
+        tx.commit();
     }
 }
