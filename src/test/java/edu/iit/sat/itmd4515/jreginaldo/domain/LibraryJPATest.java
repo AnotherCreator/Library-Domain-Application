d6273e497937656248a253dd6217b53150bada13
AnotherCreator <jreginaldo@protonmail.com>
2022-09-25 12:59:53 -0500

    [Lab 4] Add entity functionality

--- src/test/java/edu/iit/sat/itmd4515/jreginaldo/domain/LibraryJPATest.java
+++ src/test/java/edu/iit/sat/itmd4515/jreginaldo/domain/LibraryJPATest.java
@@ -2,16 +2,27 @@ package edu.iit.sat.itmd4515.jreginaldo.domain;
 
 import org.junit.jupiter.api.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.logging.Logger;
+
 public class LibraryJPATest {
+    private static EntityManagerFactory emf;
+    private static final Logger LOG = Logger.getLogger(LibraryJPATest.class.getName());
+    private EntityManager em;
+    private EntityTransaction tx;
 
     @BeforeAll
     public static void beforeAll() {
-
+        emf = Persistence.createEntityManagerFactory("itmd4515testPU");
     }
 
     @BeforeEach
     public void beforeEach() {
-
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
     }
 
     @Test
@@ -41,6 +52,6 @@ public class LibraryJPATest {
 
     @AfterAll
     public static void afterAll() {
-
+        emf.close();
     }
 }
