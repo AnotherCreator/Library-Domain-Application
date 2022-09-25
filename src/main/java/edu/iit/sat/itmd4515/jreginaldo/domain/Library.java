b2617639901def32b3654f1835e9e78856a19d64
AnotherCreator <jreginaldo@protonmail.com>
2022-09-25 12:01:21 -0500

    [Lab 4] Create equals + hash methods and add constraints to field values

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Library.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Library.java
@@ -1,7 +1,14 @@
 package edu.iit.sat.itmd4515.jreginaldo.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 public class Library {
@@ -10,16 +17,27 @@ public class Library {
 
     }
 
-    public Library(String name, String streetAdd, String phoneNum) {
+    public Library(String name, String streetAdd, String phoneNum, LocalDate established) {
         this.name = name;
         this.streetAdd = streetAdd;
         this.phoneNum = phoneNum;
+        this.established = established;
     }
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String streetAdd;
+
+    @NotBlank
     private String phoneNum;
+
+    @PastOrPresent @NotNull
+    private LocalDate established;
 
     public String getName() {
         return name;
@@ -45,12 +63,38 @@ public class Library {
         this.phoneNum = phoneNum;
     }
 
+    public LocalDate getEstablished() {
+        return established;
+    }
+
+    public void setEstablished(LocalDate established) {
+        this.established = established;
+    }
+
     @Override
     public String toString() {
         return "Library{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", streetAdd='" + streetAdd + '\'' +
                 ", phoneNum='" + phoneNum + '\'' +
+                ", established=" + established +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        if ((this.id == null || library.id == null)) {
+            return false;
+        }
+        return id.equals(library.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
