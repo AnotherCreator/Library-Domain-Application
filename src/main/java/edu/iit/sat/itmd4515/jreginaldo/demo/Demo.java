77926a65635f1171911cc9b90ba9baf597799bf6
Josh R <jreginaldo@protonmail.com>
2022-09-26 10:07:34 -0500

    [Lab 4] Add basic data to library fields

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/demo/Demo.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/demo/Demo.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
 
 public class Demo {
     public static void main(String[] args) {
@@ -17,6 +18,9 @@ public class Demo {
         Library library = new Library();
 
         library.setName("Avant-Garde Athenaeum");
+        library.setStreetAdd("Address");
+        library.setPhoneNum("111-111-1111");
+        library.setEstablished(LocalDate.now());
         System.out.println(library);
 
         em.persist(library);
