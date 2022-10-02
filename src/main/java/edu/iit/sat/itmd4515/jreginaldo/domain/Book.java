327ac82d77d115422a6be5c98b02fcaddd83f1e9
AnotherCreator <jreginaldo@protonmail.com>
2022-10-02 10:03:35 -0500

    [Lab 5] Add imports and set class as an entity

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Book.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Book.java
@@ -1,4 +1,15 @@
 package edu.iit.sat.itmd4515.jreginaldo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
 public class Book {
 }
