b9296953ebd9830fea37e183bcce7107b07ad4d6
AnotherCreator <jreginaldo@protonmail.com>
2022-10-02 11:29:05 -0500

    [Lab 5] Build Member @Entity POJO

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Member.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Member.java
@@ -12,4 +12,94 @@ import java.util.Objects;
 
 @Entity
 public class Member {
+
+    public Member() {
+
+    }
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    // 0 -- Local Resident Card Holder
+    // 1 -- Non Local Resident Card Holder
+    // 2 -- Staff Card Holder
+    private int memberType;
+    // 0 -- Not expired
+    // 1 -- Expired
+    private int isExpired;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(int memberType) {
+        this.memberType = memberType;
+    }
+
+    public int getIsExpired() {
+        return isExpired;
+    }
+
+    public void setIsExpired(int isExpired) {
+        this.isExpired = isExpired;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        if ((this.ID == null || member.ID == null)) {
+            return false;
+        }
+        return ID.equals(member.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID);
+    }
 }
