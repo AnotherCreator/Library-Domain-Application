58cb70ba890e0daf3471dd6f3d9b46d71eb7357e
AnotherCreator <jreginaldo@protonmail.com>
2022-10-02 11:22:00 -0500

    [Lab 5] Build Author @Entity POJO

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Author.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Author.java
@@ -13,4 +13,71 @@ import java.util.Objects;
 @Entity
 public class Author {
 
+    public Author() {
+
+    }
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    private String firstName;
+    private String lastName;
+    private LocalDate DOB; // Date of Birth
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "ID=" + ID +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        if ((this.ID == null || author.ID == null)) {
+            return false;
+        }
+        return ID.equals(author.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID);
+    }
 }
