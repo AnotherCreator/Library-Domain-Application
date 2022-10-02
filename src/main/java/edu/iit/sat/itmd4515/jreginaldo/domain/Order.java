7c2aee49458a307857639ee51c79ca459f2ebb0d
AnotherCreator <jreginaldo@protonmail.com>
2022-10-02 11:30:36 -0500

    [Lab 5] Build Order @Entity POJO

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Order.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Order.java
@@ -12,4 +12,62 @@ import java.util.Objects;
 
 @Entity
 public class Order {
+
+    public Order() {
+
+    }
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+    private LocalDate reservationDate;
+    private LocalDate checkOutDate;
+    private LocalDate returnDate;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(LocalDate reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        if ((this.ID == null || order.ID == null)) {
+            return false;
+        }
+        return ID.equals(order.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID);
+    }
 }
