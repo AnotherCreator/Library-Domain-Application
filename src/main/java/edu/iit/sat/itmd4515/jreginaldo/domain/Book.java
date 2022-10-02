5b4b0dac62e30bbc7fb030b51705d0a36285a1e1
AnotherCreator <jreginaldo@protonmail.com>
2022-10-02 11:22:18 -0500

    [Lab 5] Build Book @Entity POJO

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Book.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/domain/Book.java
@@ -12,4 +12,89 @@ import java.util.Objects;
 
 @Entity
 public class Book {
+
+    public Book() {
+
+    }
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long barcode;
+    private String ISBN;
+    private String title;
+    private String author;
+    private String publishedBy;
+    private String genre;
+    private LocalDate publishedDate;
+
+    public Long getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(Long barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishedBy() {
+        return publishedBy;
+    }
+
+    public void setPublishedBy(String publishedBy) {
+        this.publishedBy = publishedBy;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        if ((this.barcode == null || book.barcode == null)) {
+            return false;
+        }
+        return barcode.equals(book.barcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(barcode);
+    }
 }
