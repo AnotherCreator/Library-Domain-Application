6c34cbd5fce30ff26e8cf29817a9e293419af18e
AnotherCreator <jreginaldo@protonmail.com>
2022-10-03 11:18:59 -0500

    [Lab 5] Remove parent related functions and only keep test cases

--- src/test/java/edu/iit/sat/itmd4515/jreginaldo/domain/LibraryJPATest.java
+++ src/test/java/edu/iit/sat/itmd4515/jreginaldo/domain/LibraryJPATest.java
@@ -2,38 +2,10 @@ package edu.iit.sat.itmd4515.jreginaldo.domain;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.logging.Logger;
 
-public class LibraryJPATest {
-    private static EntityManagerFactory emf;
-    private static final Logger LOG = Logger.getLogger(LibraryJPATest.class.getName());
-    private EntityManager em;
-    private EntityTransaction tx;
-
-    @BeforeAll
-    public static void beforeAll() {
-        emf = Persistence.createEntityManagerFactory("itmd4515testPU");
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        em = emf.createEntityManager();
-        tx = em.getTransaction();
-
-        Library libraryTest = new Library("libraryTest", "TestAddress", "123-123-1234", LocalDate.now());
-
-        // Begin insertion sequence
-        tx.begin();
-        em.persist(libraryTest);
-        tx.commit();
-    }
-
+public class LibraryJPATest extends AbstractJPATest{
     @Test
     public void createTest() {
         Library createTest = new Library("createTest", "TestAddress", "123-123-1234", LocalDate.now());
@@ -106,22 +78,5 @@ public class LibraryJPATest {
         Library deleteCheck = em.find(Library.class, deleteTest.getId());
 
         assertNull(deleteCheck);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        Library deleteTest = em.createQuery(
-                "SELECT l FROM Library l WHERE l.name = 'libraryTest'", Library.class).getSingleResult();
-
-        // Begin deletion sequence
-        tx.begin();
-        em.remove(deleteTest);
-        tx.commit();
-        em.close();
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        emf.close();
     }
 }
