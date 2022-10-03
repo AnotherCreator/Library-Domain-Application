90bc8bcb91874eb2dc46b7cc30810868d05899c7
AnotherCreator <jreginaldo@protonmail.com>
2022-10-02 20:12:01 -0500

    [Lab 5] Add @ApplicationScoped

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/config/DatabaseConfig.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/config/DatabaseConfig.java
@@ -1,7 +1,9 @@
 package edu.iit.sat.itmd4515.jreginaldo.config;
 
 import javax.annotation.sql.DataSourceDefinition;
+import javax.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 @DataSourceDefinition(
         name = "java:app/jdbc/itmd4515DS",
         className = "com.mysql.cj.jdbc.MysqlDataSource",
