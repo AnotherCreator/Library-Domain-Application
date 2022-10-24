ccd4130a53d135c4119faca6395661d4c9007da7
AnotherCreator <jreginaldo@protonmail.com>
2022-10-24 18:47:26 -0500

    [Lab 6] Add abstract methods for CRUD operations

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/AbstractService.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/AbstractService.java
@@ -2,17 +2,34 @@ package edu.iit.sat.itmd4515.jreginaldo.service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 abstract class AbstractService<T> {
 
     @PersistenceContext(name = "itmd4515PU")
     protected EntityManager em;
 
-    public AbstractService() {
+    protected final Class<T> entityClass;
 
+    public AbstractService(Class<T> entityClass) {
+        this.entityClass = entityClass;
     }
 
     public void create(T entity) {
-
+        em.persist(entity);
     }
+
+    public T read(Long id) {
+        return em.find(entityClass, id);
+    }
+
+    public void update(T entity) {
+        em.merge(entity);
+    }
+
+    public void delete(T entity) {
+        em.remove(em.merge(entity));
+    }
+
+    abstract public List<T> findAll();
 }
