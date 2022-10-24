c1face2a6940fb22f83e4c86ff8d1eb47c7678b8
AnotherCreator <jreginaldo@protonmail.com>
2022-10-24 18:56:52 -0500

    [Lab 6] Add @EJB components

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/StartupSampleDataService.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/StartupSampleDataService.java
@@ -1,6 +1,7 @@
 package edu.iit.sat.itmd4515.jreginaldo.service;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import java.util.logging.Logger;
@@ -10,6 +11,12 @@ import java.util.logging.Logger;
 public class StartupSampleDataService {
 
     private static final Logger LOG = Logger.getLogger(StartupSampleDataService.class.getName());
+
+    @EJB private AuthorService authorService;
+    @EJB private BookService bookService;
+    @EJB private CheckoutService checkoutService;
+    @EJB private EmployeeService employeeService;
+    @EJB private MemberService memberService;
 
     public StartupSampleDataService() {
 
