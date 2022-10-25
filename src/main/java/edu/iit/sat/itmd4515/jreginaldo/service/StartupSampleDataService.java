73b15030f5207b8b3c4c9f58426c38212c26d35f
AnotherCreator <jreginaldo@protonmail.com>
2022-10-24 19:35:57 -0500

    [Lab 6] Invoke CRUD methods within StartupSampleDataService.java

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/StartupSampleDataService.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/StartupSampleDataService.java
@@ -1,9 +1,16 @@
 package edu.iit.sat.itmd4515.jreginaldo.service;
 
+import edu.iit.sat.itmd4515.jreginaldo.domain.Author;
+import edu.iit.sat.itmd4515.jreginaldo.domain.Book;
+import edu.iit.sat.itmd4515.jreginaldo.domain.Checkout;
+import edu.iit.sat.itmd4515.jreginaldo.domain.Member;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.logging.Logger;
 
 @Startup
@@ -25,5 +32,37 @@ public class StartupSampleDataService {
     @PostConstruct
     private void postConstruct() {
         LOG.info("Inside StartupSampleDataService.postConstruct method");
+
+        // Entities that DO NOT OWN relationships
+        Member member1 = new Member("Ocean", "Man", "111-111-1111", "111 Ocean Drive",
+                0, 0);
+
+        memberService.create(member1);
+
+        // Entities that OWN relationships
+        Checkout checkout1 = new Checkout(LocalDate.of(2022, Month.JANUARY, 1), // Past or present
+                LocalDate.of(2022, Month.NOVEMBER, 1),  // Future or present
+                LocalDate.of(2022, Month.DECEMBER, 2)); // Future
+        Checkout checkout2 = new Checkout(LocalDate.of(2022, Month.JANUARY, 2), // Past or present
+                LocalDate.of(2022, Month.NOVEMBER, 1),  // Future or present
+                LocalDate.of(2022, Month.DECEMBER, 2)); // Future
+
+        member1.getCheckoutSet().add(checkout1);
+        member1.getCheckoutSet().add(checkout2);
+
+        checkout1.setMember(member1);
+        checkout2.setMember(member1);
+
+        checkoutService.create(checkout1);
+        checkoutService.create(checkout2);
+
+        for (Member member : memberService.findAll()) {
+            LOG.info(" ========== MEMBER ========== \n" + member.toString());
+        }
+
+        for (Checkout checkout : checkoutService.findAll()) {
+            LOG.info(" ========== CHECKOUT ========== \n" + checkout.toString());
+            LOG.info(" ========== CHECKOUT MEMBER ========== \n" + checkout.getMember().toString());
+        }
     }
 }
