14462cb9ed015616540f808d2572e440e2b50a75
AnotherCreator <jreginaldo@protonmail.com>
2022-10-29 17:05:52 -0500

    [Lab 7] Add MemberController

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/web/MemberController.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/web/MemberController.java
@@ -1,4 +1,25 @@
 package edu.iit.sat.itmd4515.jreginaldo.web;
 
+import edu.iit.sat.itmd4515.jreginaldo.domain.Member;
+import edu.iit.sat.itmd4515.jreginaldo.service.MemberService;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.util.logging.Logger;
+
+@Named
+@RequestScoped
 public class MemberController {
+
+    private static final Logger LOG = Logger.getLogger(MemberController.class.getName());
+
+    @EJB private MemberService memberService;
+    private Member member;
+
+    public MemberController() {
+
+    }
+
+
 }
