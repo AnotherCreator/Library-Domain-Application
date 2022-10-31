bf9e16f8ecbfd31d5edf391b4e17e89203233416
AnotherCreator <jreginaldo@protonmail.com>
2022-10-31 18:34:52 -0500

    [Lab 7] Display final confirmation page showing inputted values

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/web/MemberController.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/web/MemberController.java
@@ -3,6 +3,7 @@ package edu.iit.sat.itmd4515.jreginaldo.web;
 import edu.iit.sat.itmd4515.jreginaldo.domain.Member;
 import edu.iit.sat.itmd4515.jreginaldo.service.MemberService;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
@@ -14,12 +15,33 @@ public class MemberController {
 
     private static final Logger LOG = Logger.getLogger(MemberController.class.getName());
 
-    @EJB private MemberService memberService;
+    @EJB
+    private MemberService memberService;
     private Member member;
 
     public MemberController() {
-
     }
 
+    @PostConstruct
+    private void postConstruct() {
+        LOG.info("MemberController.postConstuct");
 
+        member = new Member();
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public String saveMember() {
+        LOG.info("Inside saveMember with " + this.member.toString());
+
+        memberService.create(member);
+
+        return "confirmation.xhtml";
+    }
 }
