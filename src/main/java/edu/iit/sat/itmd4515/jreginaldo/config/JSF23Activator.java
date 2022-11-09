ba2496fb347a2090ddfd6828853f6656a9b532f2
AnotherCreator <jreginaldo@protonmail.com>
2022-11-08 22:13:27 -0600

    [Lab 8] Create JSF23Activator as demo'd in class

---
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/config/JSF23Activator.java
@@ -0,0 +1,10 @@
+package edu.iit.sat.itmd4515.jreginaldo.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class JSF23Activator {
+
+}
