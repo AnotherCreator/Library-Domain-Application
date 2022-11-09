afb7235009ea4e890c6a23018e9bfc8f71b5e1a4
AnotherCreator <jreginaldo@protonmail.com>
2022-11-08 19:03:03 -0600

    [Lab 8] Create User + Group file as demo'd in lecture

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/security/Group.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/security/Group.java
@@ -1,4 +1,56 @@
 package edu.iit.sat.itmd4515.jreginaldo.security;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+// General structure as demo'd in class
+
+@Entity
+@Table(name = "sec_group")
 public class Group {
+    public Group() {
+
+    }
+
+    /*
+            ========== OBJECT RELATED VARIABLES ==========
+    */
+    @Id
+    private String groupName;
+    private String groupDesc;
+
+    // User (Owner) -- Group (Owned)
+
+    @ManyToMany(mappedBy = "groups")
+    private List<User> users = new ArrayList<>();
+
+    /*
+        ========== GETTER / SETTER ==========
+    */
+    public String getGroupName() {
+        return groupName;
+    }
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getGroupDesc() {
+        return groupDesc;
+    }
+
+    public void setGroupDesc(String groupDesc) {
+        this.groupDesc = groupDesc;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/security/User.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/security/User.java
@@ -1,4 +1,71 @@
 package edu.iit.sat.itmd4515.jreginaldo.security;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// General structure as demo'd in class
+
+@Entity
+@Table(name = "sec_user")
 public class User {
+    public User() {
+
+    }
+
+    /*
+        ========== OBJECT RELATED VARIABLES ==========
+     */
+    @Id
+    private String userName;
+    private String password;
+
+    // Many users to belong to many groups
+    // E.g. A patron can be a member, employee, and admin
+
+    @ManyToMany
+    // User (Owner) -- Group (Owned)
+    @JoinTable(name = "sec_user_groups",
+                joinColumns = @JoinColumn(name = "USERNAME"),
+                inverseJoinColumns = @JoinColumn(name = "GROUPNAME"))
+    private List<Group> groups = new ArrayList<>();
+
+    /*
+        ========== GETTER / SETTER ==========
+     */
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    /*
+        ========== HELPER METHODS ==========
+     */
+    public void addGroup(Group g) {
+        this.groups.add(g);
+        g.getUsers().add(this);
+    }
+
+    public void removeGroup(Group g) {
+        this.groups.remove(g);
+        g.getUsers().remove(this);
+    }
 }
