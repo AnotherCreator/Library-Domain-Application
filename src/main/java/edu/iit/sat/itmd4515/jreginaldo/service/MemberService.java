5630b30851e6ffb0e70493b68ccac6bfc4792228
AnotherCreator <jreginaldo@protonmail.com>
2022-11-22 18:35:56 -0600

    [Lab 10] Fix page title issue

--- src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/MemberService.java
+++ src/main/java/edu/iit/sat/itmd4515/jreginaldo/service/MemberService.java
@@ -1,13 +1,17 @@
 package edu.iit.sat.itmd4515.jreginaldo.service;
 
 import edu.iit.sat.itmd4515.jreginaldo.domain.Member;
+import edu.iit.sat.itmd4515.jreginaldo.web.MemberController;
+
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Named
 @Stateless
 public class MemberService extends AbstractService<Member> {
+    private static final Logger LOG = Logger.getLogger(MemberController.class.getName());
 
     public MemberService() {
         super(Member.class);
@@ -21,5 +25,19 @@ public class MemberService extends AbstractService<Member> {
     public Member findByUsername(String username) {
         return em.createNamedQuery("Member.findByUsername", Member.class)
                 .setParameter("username", username).getSingleResult();
+    }
+
+    public void updateMember(Member member) {
+//        Member memberReference = em.getReference(Member.class, member.getID());
+//
+//        memberReference.setIsExpired(member.getIsExpired());
+//
+//        em.merge(memberReference);
+    }
+
+    public void deleteMember(Member member) {
+        Member memberReference = em.getReference(Member.class, member.getID());
+
+        em.remove(memberReference);
     }
 }
